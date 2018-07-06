911a6f46b70ff71bb8bea27ba11a73559c293de3
Unknown <crisfox95@gmail.com>
2018-07-06 20:37:37 -0300

    agregando chat

---
+++ app/src/main/java/com/example/dh/entregableandroid2/view/ActivityChat.java
@@ -0,0 +1,15 @@
+package com.example.dh.entregableandroid2.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dh.entregableandroid2.R;
+
+public class ActivityChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
